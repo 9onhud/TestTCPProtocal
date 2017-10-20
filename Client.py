0cd5b32a3cc13efe6981751396317656c3ab49d9
Techasit Anukrua <techasit.a@ku.th>
2017-10-20 22:51:50 +0700

    ver. can use multiple client 2

--- Client.py
+++ Client.py
@@ -5,25 +5,31 @@ def start_client():
     port = 5098
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((socket.gethostname(), port))
+    try:
+        client_socket.connect((socket.gethostname(), port))
 
-    # send name to server
-    name = input("My name: ")
-    client_socket.send(name.encode())
+        # send name to server
+        name = input("My name: ")
+        client_socket.send(name.encode())
 
-    while True:
-        data = input("Message :")
-        if data in "Qq":
-            break
+        status = client_socket.recv(1024).decode()
+        if status == "ready":
+            while True:
+                data = input("Message :")
+                if data in "Qq":
+                    break
 
-        client_socket.send(data.encode())
-        print("Send :" + data)
+                client_socket.send(data.encode())
+                print("Send :" + data)
 
-        data_from_server = client_socket.recv(1024).decode()
-        print("Receive :" + data_from_server)
-
-    client_socket.close()
-    print("Client is disconnect")
+                data_from_server = client_socket.recv(1024).decode()
+                print("Receive :" + data_from_server)
+    except Exception as e:
+        print("You aren't connect.")
+        print(e)
+    finally:
+        client_socket.close()
+        print("Client is disconnect")
 
 
 if __name__ == "__main__":
--- Server.py
+++ Server.py
@@ -33,6 +33,35 @@ def transferData(client):
 
 def start_server():
 
+    # wait 4 client(Player) connect and receive name from client
+    def connect_client():
+        threads = []
+        for i in range(player_number):
+            client, address = server_socket.accept()
+            client_name = client.recv(1024).decode()  # wait to receive name from client
+
+            threads.append(MyThread(client_name, client, address))
+            print("Client: " + threads[i].name + " Connect")
+        return threads
+
+    # send to all client that all client are connection
+    def send_ready():
+        for thread in client_threads:
+            thread.client.send("ready".encode())
+
+    # run all thread(client)
+    def start_threads():
+        for thread in client_threads:
+            thread.start()
+
+    # if all client close connect, server close connect
+    def check_connect_for_close():
+        while True:
+            if threading.active_count() == 1:
+                server_socket.close()
+                print("Server is disconnect")
+                break
+
     port = 5098
     player_number = 4
 
@@ -40,27 +69,13 @@ def start_server():
     server_socket.bind((socket.gethostname(), port))
 
     print("Server is wait for client's connect")
-    server_socket.listen(player_number)  # wait for 4 player
+    server_socket.listen(player_number)  # can connect 4 client
 
-    # wait 4 client(Player) connect and receive name from client
-    client_threads = []
-    for i in range(player_number):
-        client, address = server_socket.accept()
-        client_name = client.recv(1024).decode()  # wait to receive name from client
+    client_threads = connect_client()   # wait to connect 4 clients
+    send_ready()        # send ready to client
+    start_threads()     # start game
 
-        thread = MyThread(client_name, client, address)
-        client_threads.append(thread)
-        print("Client:"+client_threads[i].name+" Connect")
-
-    # run all thread(client)
-    for thread in client_threads:
-        thread.start()
-
-    while True:
-        if threading.active_count() == 1:
-            server_socket.close()
-            print("Server is disconnect")
-            break
+    check_connect_for_close()
 
 
 if __name__ == "__main__":
